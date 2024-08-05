8df0baa59a3707d3730eb806e20a8a001084d6d8
Andrey Djurinschi <djurinschi.andreiai@gmail.com>
2024-08-06 01:34:18 +0300

    initial commit

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var person = new TestViewModel
+            {
+                Name = "Andrey",
+                Age = 20,
+                Description = "2 grade Student",
+                DateofBirth = new DateTime(2004, 07, 09, 06, 15, 30),
+            };
+
+            return View(person);
+        }
+    }
+}
